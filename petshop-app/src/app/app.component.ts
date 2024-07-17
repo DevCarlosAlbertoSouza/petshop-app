e789f60d677717df92237d6cf69089b0e08598f9
José Vitor - Casa <josevitornm2014@outlook.com>
2024-07-17 20:55:31 -0300

    Criação do componente e modificação do Header

--- petshop-app/src/app/app.component.ts
+++ petshop-app/src/app/app.component.ts
@@ -3,13 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { PetcareComponent } from "./componentes/petcare/petcare.component";
 import { LoginComponent } from "./componentes/login/login.component";
 import { NgOptimizedImage } from '@angular/common';
+import { HeaderComponent } from "./componentes/header/header.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet,NgOptimizedImage, PetcareComponent, LoginComponent]
+    imports: [RouterOutlet, NgOptimizedImage, PetcareComponent, LoginComponent, HeaderComponent]
 })
 export class AppComponent {
   title = 'petshop-app';
