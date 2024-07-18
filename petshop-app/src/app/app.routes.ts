8f1f5603ded3ccb707fd4b238357c568d662da95
José Vitor - Casa <josevitornm2014@outlook.com>
2024-07-17 22:28:49 -0300

    Rota

--- petshop-app/src/app/app.routes.ts
+++ petshop-app/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './componentes/login/login.component';
 import { AgendaComponent } from './componentes/agenda/agenda.component';
 import { PetcareComponent } from './componentes/petcare/petcare.component';
+import { CadastroClienteDadosPessoaisComponent } from './componentes/cadastro/cadastro-cliente-dados-pessoais/cadastro-cliente-dados-pessoais.component';
 
 export const routes: Routes = [
 
@@ -24,8 +25,4 @@ export const routes: Routes = [
         path: 'cadastro-cliente-dados-pessoais',
         component: CadastroClienteDadosPessoaisComponent,
     },
-    {
-        path: 'cadastro',
-        component:CadastroComponent
-    }
 ];
